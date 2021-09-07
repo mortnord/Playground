bc0ccb53d3669593e30cb2f7e847b8743e1e39e2
karlj <karljlia@gmail.com>
2021-09-07 18:26:20 +0200

    out of health tilbakestille lister

--- MainCharacters.py
+++ MainCharacters.py
@@ -9,9 +9,16 @@ class MainCharacters:
     def lose_health(self, amount):
         # Metoden man kaller for å miste liv
         self.health = self.health - amount
-        if self.health < 0:
+        if self.health <= 0:
             self.health = 0
             print("Out of health")
+            self.player_sprite.center_x = 50 # Sentrerer spiller til utgangspunktet når tom for liv
+            self.player_sprite.center_y = 50
+            self.health = 12                 # Tilbakestiller liv til 12 når tom for liv
+            self.inventory_character.get_inventory_contents().clear()
+            for x in range(len(self.inventory_character.inventory_contents_sprite_list)): # Tilbakestiller inventorylist
+                self.inventory_character.items_in_food_inventory = self.inventory_character.items_in_food_inventory - 1
+                self.inventory_character.inventory_contents_sprite_list.pop()
         pass
 
     def gain_health(self, amount):
